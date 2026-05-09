dec6c0e65127dcf2ddbcaa5d85aeb35d92f7a103
Samuel Vitor <samuelvitorengqui@gmail.com>
2026-05-09 08:20:59 -0300

    feat: adiciona e busca dados quando o usuário aperta enter

--- src/js/index.js
+++ src/js/index.js
@@ -5,23 +5,33 @@ const inputSearch = document.getElementById('input-search');
 const btnSearch = document.getElementById('btn-search');
 const profileResults = document.querySelector('.profile-results');
 
-btnSearch.addEventListener('click', async () => {
-    const userName = inputSearch.value;
+async function getUserProfile() {
+    const userName = inputSearch.value.trim();
     if (!userName) {
         alert('Por favor, digite um nome de usuário do GitHub.');
         profileResults.innerHTML = "";
         return;
     }
+
     profileResults.innerHTML = `<p class="loading">Carregando...</p>`;
+
     try {
         const userData = await fetchGithubUser(userName);
         const userRepos = await fetchGithubRepos(userName);
         console.log(userRepos);
         renderProfile(userData, userRepos, profileResults);
-        
     } catch (error) {
         console.error('Erro ao buscar o perfil do usuário:', error);
         alert('Usuário não encontrado. Por favor, verifique o nome de usuário e tente novamente.');
         profileResults.innerHTML = "";
+    }
+}
+
+btnSearch.addEventListener('click', getUserProfile);
+
+inputSearch.addEventListener('keypress', (event) => {
+    if (event.key === 'Enter') {
+        event.preventDefault();
+        getUserProfile();
     }
 });
